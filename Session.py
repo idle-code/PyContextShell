c64dcc17db9124f6ae41dfdc15e851dd36945e5d
Pawel Zukowski <p.z.idlecode@gmail.com>
2017-08-05 09:00:34 +0200

    Added explicit types to Session interface methods

--- Session.py
+++ Session.py
@@ -9,28 +9,25 @@ class Session:
         pass
 
     def execute(self, target: NodePath, action, *arguments):
-        raise NotImplementedError("TreeView.execute method was not overridden")
+        raise NotImplementedError("Session.execute method was not overridden")
 
-    def get(self, target):
-        target = NodePath.cast(target)
+    def get(self, target: NodePath):
         return self.execute(target, 'get')
 
-    def set(self, target, new_value):
-        target = NodePath.cast(target)
+    def set(self, target: NodePath, new_value):
         return self.execute(target, 'set', new_value)
 
-    def list(self, target):
-        target = NodePath.cast(target)
+    def list(self, target: NodePath):
         return self.execute(target, 'list')
 
-    def exists(self, target):
+    def exists(self, target: NodePath):
         target = NodePath.cast(target)
         return self.execute(target.base_path, 'exists', target.base_name)
 
-    def create(self, target, value=None):
+    def create(self, target: NodePath, value=None):
         target = NodePath.cast(target)
         return self.execute(target.base_path, 'create', target.base_name, value)
 
-    def remove(self, target):
+    def remove(self, target: NodePath):
         target = NodePath.cast(target)
         return self.execute(target.base_path, 'remove', target.base_name)
