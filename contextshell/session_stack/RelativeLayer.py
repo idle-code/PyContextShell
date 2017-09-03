4caba93ec2e27905a87f732f461d20b96175b96e
Pawel Zukowski <p.z.idlecode@gmail.com>
2017-09-03 12:25:23 +0200

    RelativeLayer implementation now uses VirtualNodeLayer

--- contextshell/session_stack/RelativeLayer.py
+++ contextshell/session_stack/RelativeLayer.py
@@ -1,42 +1,49 @@
+from contextshell.session_stack.VirtualNodeLayer import VirtualNodeLayer
 from contextshell.session_stack.SessionLayer import *
 from typing import List
 
 
-class RelativeLayer(SessionLayer):
+class RelativeLayer(VirtualNodeLayer):
     """Layer responsible for providing relative navigation in tree"""
 
     def __init__(self, backing_path: NodePath, start_path: NodePath):
-        super().__init__()
-        self.backing_path = NodePath.cast(backing_path)
-        self.start_path = NodePath.cast(start_path)
+        super().__init__(backing_path)
+        self.current_path: NodePath = NodePath.cast(start_path)
 
-    @property
-    def current(self):
-        if not self.exists(self.backing_path):
-            self.create(self.backing_path, self.start_path)
-        return self.get(self.backing_path)
+    def on_get(self):
+        return self.current_path
+
+    def on_set(self, new_value: NodePath):
+        new_current_path = NodePath.cast(new_value)
+        if not self.exists(new_current_path):
+            raise ValueError("New current path doesn't exists")
+
+        if new_current_path.is_absolute:
+            self.current_path = new_value
+        else:
+            self.current_path = NodePath.join(self.current_path, new_value)
 
     def _rewrite_path(self, path: NodePath):
         path = NodePath.cast(path)
         if path.is_absolute:
             return path
         else:
-            return NodePath.join(self.current, path)
+            return NodePath.join(self.current_path, path)
 
     def get(self, path: NodePath):
-        return self.next_layer.get(self._rewrite_path(path))
+        return super().get(self._rewrite_path(path))
 
     def set(self, path: NodePath, new_value):
-        return self.next_layer.set(self._rewrite_path(path), new_value)
+        return super().set(self._rewrite_path(path), new_value)
 
     def list(self, path: NodePath) -> List[NodePath]:
-        return self.next_layer.list(self._rewrite_path(path))
+        return super().list(self._rewrite_path(path))
 
     def exists(self, path: NodePath) -> bool:
-        return self.next_layer.exists(self._rewrite_path(path))
+        return super().exists(self._rewrite_path(path))
 
     def create(self, path: NodePath, value=None):
-        return self.next_layer.create(self._rewrite_path(path), value)
+        return super().create(self._rewrite_path(path), value)
 
     def remove(self, path: NodePath):
-        return self.next_layer.remove(self._rewrite_path(path))
+        return super().remove(self._rewrite_path(path))
--- tests/session_stack/RelativeLayerTests.py
+++ tests/session_stack/RelativeLayerTests.py
@@ -13,12 +13,8 @@ class BasicSessionLayerTests(TestBases.SessionLayerTestsBase):
         return RelativeLayer(backing_path, start_path)
 
 
-@unittest.skip("Fix when VirtualNodeLayer will be available")
-class RelativeLayerTests(unittest.TestCase):
-    def setUp(self):
-        root = TreeRoot()
-        # Create backing and test nodes
-        session = root.create_session()
+class RelativeLayerTests(TestBases.LayerTestsBase):
+    def prepare_layer(self, session: SessionLayer) -> SessionLayer:
         session.create(NodePath(".first"), 1)
         session.create(NodePath(".first.second"), 2)
         session.create(NodePath(".first.second.third"), 3)
@@ -27,100 +23,72 @@ class RelativeLayerTests(unittest.TestCase):
         session.create(NodePath(".first.foo"), 'foo1')
         session.create(NodePath(".foo"), 'foo0')
 
-        # Setup session stack (to push TemporarySession on top)
-        self.storage_layer = root.create_session()
-        session_stack = SessionStack(self.storage_layer)
-        self.backing_path = NodePath('.current_path')
-        session_stack.push(RelativeLayer(self.backing_path, NodePath('.first')))
-        self.session = session_stack
+        self.current_path = NodePath('.current_path')
+        return RelativeLayer(self.current_path, NodePath('.first'))
 
     def test_current_path_exists(self):
-        self.assertTrue(self.session.exists(self.backing_path))
+        self.assertTrue(self.tested_layer.exists(self.current_path))
 
     def test_get_current_path(self):
-        current_path = self.session.get(self.backing_path)
+        current_path = self.tested_layer.get(self.current_path)
         self.assertEqual(current_path, NodePath(".first"))
 
     def test_set_current_path(self):
-        self.session.set(self.backing_path, NodePath('.'))
-        current_path = self.session.get(self.backing_path)
+        self.tested_layer.set(self.current_path, NodePath('.'))
+        current_path = self.tested_layer.get(self.current_path)
         self.assertEqual(current_path, NodePath('.'))
 
-    def test_set_current_path_relative(self):
-        self.session.set(self.backing_path, NodePath('second'))
-        current_path = self.session.get(self.backing_path)
-        self.assertEqual(current_path, NodePath('second'))
-        with self.assertRaises(NameError):
-            self.session.get(NodePath('second'))
+    def test_set_current_path_bad_type(self):
+        with self.assertRaises(ValueError):
+            self.tested_layer.set(self.current_path, 3.14)
 
-    def test_absolute_get(self):
-        foo_value = self.session.get(NodePath('.first.second.foo'))
-        self.assertEqual('foo2', foo_value)
+    def test_set_current_path_relative(self):
+        self.tested_layer.set(self.current_path, NodePath('second'))
+        current_path = self.tested_layer.get(self.current_path)
+        self.assertEqual(current_path, NodePath('.first.second'))
+        self.assertTrue(self.tested_layer.exists(NodePath('third')))
+
+    def test_set_current_path_nonexistent(self):
+        with self.assertRaises(ValueError):
+            self.tested_layer.set(self.current_path, NodePath('relative'))
+        with self.assertRaises(ValueError):
+            self.tested_layer.set(self.current_path, NodePath('.absolute'))
 
     def test_relative_get(self):
-        foo_value = self.session.get(NodePath('foo'))
+        foo_value = self.tested_layer.get(NodePath('foo'))
         self.assertEqual('foo1', foo_value)
 
     def test_relative_get_after_change(self):
-        self.session.set(self.backing_path, NodePath('.first.second.third'))
-        current_path = self.session.get(self.backing_path)
-
-        foo_value = self.session.get(NodePath('foo'))
+        self.tested_layer.set(self.current_path, NodePath('.first.second.third'))
+        foo_value = self.tested_layer.get(NodePath('foo'))
         self.assertEqual('foo3', foo_value)
 
-    def test_absolute_set(self):
-        self.session.set(NodePath('.first.second.foo'), 'foobar')
-        foo_value = self.storage_layer.get(NodePath('.first.second.foo'))
-        self.assertEqual('foobar', foo_value)
-
     def test_relative_set(self):
-        self.session.set(NodePath('second.foo'), 'foobar')
+        self.tested_layer.set(NodePath('second.foo'), 'foobar')
         foo_value = self.storage_layer.get(NodePath('.first.second.foo'))
         self.assertEqual('foobar', foo_value)
-
-    def test_absolute_list(self):
-        first_nodes = self.session.list(NodePath('.first'))
-        expected_nodes = [NodePath('.first.second'),
-                          NodePath('.first.foo')]
-        self.assertListEqual(expected_nodes, first_nodes)
 
     def test_relative_list(self):
-        second_nodes = self.session.list(NodePath('second'))
+        second_nodes = self.tested_layer.list(NodePath('second'))
         expected_nodes = [NodePath('.first.second.third'),
                           NodePath('.first.second.foo')]
         self.assertListEqual(expected_nodes, second_nodes)
 
-    def test_absolute_exists(self):
-        self.assertTrue(self.session.exists(NodePath('.first.foo')))
-        self.assertFalse(self.session.exists(NodePath('.first.bar')))
-
     def test_relative_exists(self):
-        self.assertTrue(self.session.exists(NodePath('second')))
-        self.assertFalse(self.session.exists(NodePath('second.bar')))
-
-    def test_absolute_create(self):
-        self.session.create(NodePath(".first.second.bar"), 'bar')
-        self.assertEqual('bar', self.storage_layer.get(NodePath('.first.second.bar')))
+        self.assertTrue(self.tested_layer.exists(NodePath('second')))
+        self.assertFalse(self.tested_layer.exists(NodePath('second.bar')))
 
     def test_relative_create(self):
-        self.session.create(NodePath("second.bar"), 'bar')
+        self.tested_layer.create(NodePath("second.bar"), 'bar')
         self.assertEqual('bar', self.storage_layer.get(NodePath('.first.second.bar')))
 
-    def test_absolute_remove(self):
-        self.session.remove(NodePath(".first.second.foo"))
-        self.assertFalse(self.storage_layer.exists(NodePath('.first.second.foo')))
-
     def test_relative_remove(self):
-        self.session.remove(NodePath("second.foo"))
+        self.tested_layer.remove(NodePath("second.foo"))
         self.assertFalse(self.storage_layer.exists(NodePath('.first.second.foo')))
 
 
 @unittest.skip("TODO next")
 class RelativeLayerActionsTests(unittest.TestCase):
-    def setUp(self):
-        self.root = TreeRoot()
-        self.session = TreeRoot.create_session()
-
     def test_cd(self):
         raise NotImplementedError()
 
