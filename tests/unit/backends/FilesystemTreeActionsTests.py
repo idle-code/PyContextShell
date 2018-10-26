36994a269d3bc2f6fc268f26213fb01e725f00de
Pawel Zukowski <p.z.idlecode@gmail.com>
2018-10-26 22:09:19 +0200

    Almost all basic filesystem action tests

--- tests/unit/backends/FilesystemTreeActionsTests.py
+++ tests/unit/backends/FilesystemTreeActionsTests.py
@@ -28,27 +28,25 @@ class FilesystemTestsBase(unittest.TestCase):
             file.write(contents if contents else '')
 
     def create_directory(self, path):
-        os.mkdir(self._make_test_path(path))
+        full_path = self._make_test_path(path)
+        full_path.mkdir(parents=True)
 
     def setUp(self):
         super().setUp()
         temp_dir_suffix = type(self).__name__
         self.test_directory = tempfile.TemporaryDirectory(temp_dir_suffix)
+        self.backend = create_filesystem_tree(self.test_directory.name)
 
     def tearDown(self):
         self.test_directory.cleanup()
         super().tearDown()
 
-
-class ContainsActionTests(FilesystemTestsBase):
-    @property
-    def backend(self):
-        return create_filesystem_tree(self.test_directory.name)
-
     def execute(self, target, action, *args, **kwargs):
         args_tree = pack_argument_tree(*args, **kwargs)
         return self.backend.execute(np(target), np(action), args_tree)
 
+
+class ContainsActionTests(FilesystemTestsBase):
     def test_existing_file(self):
         self.create_file('file')
 
@@ -81,3 +79,88 @@ class ContainsActionTests(FilesystemTestsBase):
         exists = self.execute(".", "contains", 'parent.path')
 
         self.assertFalse(exists)
+
+
+class GetActionTests(FilesystemTestsBase):
+    def test_empty_file(self):
+        self.create_file('file')
+
+        file_value = self.execute(".file", "get")
+
+        self.assertEqual(file_value, '')
+
+    def test_file(self):
+        self.create_file('file', 'DATA')
+
+        file_value = self.execute(".file", "get")
+
+        self.assertEqual(file_value, 'DATA')
+
+    def test_directory(self):
+        self.create_directory('dir')
+
+        dir_value = self.execute(".dir", "get")
+
+        self.assertEqual(dir_value, None)
+
+
+class IsActionTests(FilesystemTestsBase):
+    def test_directory_is_file(self):
+        self.create_directory('dir')
+
+        is_file = self.execute(".dir", "is.file")
+
+        self.assertFalse(is_file)
+
+    def test_file_is_file(self):
+        self.create_file('file')
+
+        is_file = self.execute(".file", "is.file")
+
+        self.assertTrue(is_file)
+
+    def test_directory_is_directory(self):
+        self.create_directory('dir')
+
+        is_dir = self.execute(".dir", "is.directory")
+
+        self.assertTrue(is_dir)
+
+    def test_file_is_directory(self):
+        self.create_file('file')
+
+        is_dir = self.execute(".file", "is.directory")
+
+        self.assertFalse(is_dir)
+
+
+class ListActionTests(FilesystemTestsBase):
+    def test_list_file(self):
+        self.create_file('file')
+
+        file_list = self.execute(".file", "list")
+
+        self.assertEqual(file_list, [])
+
+    def test_list_empty_directory(self):
+        self.create_directory('dir')
+
+        file_list = self.execute(".dir", "list")
+
+        self.assertEqual(file_list, [])
+
+    def test_list_directory_with_file_and_dirs(self):
+        self.create_directory('parent/dir')
+        self.create_file('parent/file')
+
+        file_list = self.execute(".parent", "list")
+
+        self.assertIn('dir', file_list)
+        self.assertIn('file', file_list)
+
+
+class ListActionsActionTests(FilesystemTestsBase):
+    def test_list_all(self):
+        action_list = self.execute(".", "list.actions")
+
+        self.assertSetEqual({'contains', 'get', 'is', 'list'}, set(action_list))
