14b231194a8307b4e40d152a8b7a293d983fb1da
Pawel Zukowski <p.z.idlecode@gmail.com>
2018-10-27 13:19:17 +0200

    'get' and 'create' action tests for node tree

--- tests/unit/backends/NodeTreeActionsTests.py
+++ tests/unit/backends/NodeTreeActionsTests.py
@@ -9,9 +9,17 @@ def create_node_tree(*args, **kwargs):
 
 
 class NodeTreeActionsTestsBase(ActionTestsBase):
-    def create_node(self, name, value=None):
-        node_path = NodePath(name, absolute=True)
+    def create_node(self, path, value=None):
+        node_path = NodePath(path, absolute=True)
         self.backend.create(node_path, value)
+
+    def get_node_value(self, path):
+        node_path = NodePath(path, absolute=True)
+        return self.backend.get(node_path)
+
+    def node_exists(self, path) -> bool:
+        node_path = NodePath(path, absolute=True)
+        return self.backend.contains(node_path)
 
     def create_backend(self):
         return create_node_tree()
@@ -43,3 +51,52 @@ class ContainsActionTests(NodeTreeActionsTestsBase):
         exists = self.execute(".", "contains", 'parent.node')
 
         self.assertFalse(exists)
+
+
+class GetActionTests(NodeTreeActionsTestsBase):
+    def test_empty(self):
+        value = None
+        self.create_node('node', value)
+
+        value = self.execute(".node", "get")
+
+        self.assertEqual(value, value)
+
+    def test_str(self):
+        value = 'DATA'
+        self.create_node('node', value)
+
+        value = self.execute(".node", "get")
+
+        self.assertEqual(value, value)
+
+    def test_int(self):
+        value = 123
+        self.create_node('node', value)
+
+        value = self.execute(".node", "get")
+
+        self.assertEqual(value, value)
+
+
+class CreateActionTests(NodeTreeActionsTestsBase):
+    def test_empty(self):
+        self.execute(".", "create", "node")
+
+        self.assertTrue(self.node_exists('node'))
+        node_value = self.get_node_value('node')
+        self.assertEqual(node_value, None)
+
+    def test_default_string_value(self):
+        self.execute(".", "create", "node", "default value")
+
+        self.assertTrue(self.node_exists('node'))
+        node_value = self.get_node_value('node')
+        self.assertEqual(node_value, "default value")
+
+    def test_default_int_value(self):
+        self.execute(".", "create", "node", 321)
+
+        self.assertTrue(self.node_exists('node'))
+        node_value = self.get_node_value('node')
+        self.assertEqual(node_value, 321)
