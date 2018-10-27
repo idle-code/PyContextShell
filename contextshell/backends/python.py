b0773af1efb3e6bc00577ad52913e1a69a5e6d6f
Pawel Zukowski <p.z.idlecode@gmail.com>
2018-10-27 20:54:21 +0200

    'get' and 'set' actions of python object backend

--- contextshell/backends/python.py
+++ contextshell/backends/python.py
@@ -12,11 +12,15 @@ class PythonObjectTree(BuiltinExecutor):
         self.root_object = root_object
 
         self.register_builtin_action(action_from_function(self.contains_action))
+        self.register_builtin_action(action_from_function(self.get_action))
+        self.register_builtin_action(action_from_function(self.set_action))
 
     @staticmethod
     def _resolve_to_object(start_obj, path: NodePath):
         current_obj = start_obj
         for part in path:
+            if part.startswith("_"):
+                return None
             current_obj = getattr(current_obj, part, None)
             if callable(current_obj):
                 return None
@@ -26,5 +30,12 @@ class PythonObjectTree(BuiltinExecutor):
 
     def contains_action(self, target: NodePath, name: NodePath) -> bool:
         target_obj = self._resolve_to_object(self.root_object, target)
-        assert target_obj
         return self._resolve_to_object(target_obj, NodePath(name)) is not None
+
+    def get_action(self, target: NodePath) -> bool:
+        target_obj = self._resolve_to_object(self.root_object, target)
+        return target_obj
+
+    def set_action(self, target: NodePath, new_value):
+        parent_obj = self._resolve_to_object(self.root_object, target.base_path)
+        setattr(parent_obj, target.base_name, new_value)
--- tests/unit/backends/PythonObjectTreActionsTests.py
+++ tests/unit/backends/PythonObjectTreActionsTests.py
@@ -4,30 +4,43 @@ from abc import ABC, abstractmethod
 from .bases import ActionTestsBase
 
 
-class PythonObjectTreeTestsBase(ActionTestsBase, ABC):
+class TestSubObject:
+    field = 'SUB'
+
+
+class TestRoot:
+    field = 'text'
+
+    def __init__(self):
+        self.sub = TestSubObject()
+        self._prop = 123
+
+    @property
+    def prop(self):
+        return self._prop
+
+    @prop.setter
+    def prop(self, new_value):
+        self._prop = new_value
+
+    def method(self):
+        pass
+
+
+class PythonObjectTreeTestsBase(ActionTestsBase):
     def create_backend(self):
         from contextshell.backends.python import PythonObjectTree
         return PythonObjectTree(self.create_root_object())
 
-    @abstractmethod
     def create_root_object(self):
-        raise NotImplementedError()
+        return TestRoot()
+
+    @property
+    def root(self):
+        return self.backend.root_object
 
 
 class ContainsActionTests(PythonObjectTreeTestsBase):
-    def create_root_object(self):
-        class TestRoot:
-            field = 'text'
-
-            @property
-            def prop(self):
-                return 123
-
-            def method(self):
-                pass
-
-        return TestRoot()
-
     def test_nonexistent(self):
         obj_exists = self.execute('.', 'contains', 'nonexistent')
 
@@ -43,7 +56,46 @@ class ContainsActionTests(PythonObjectTreeTestsBase):
 
         self.assertTrue(field_exists)
 
+    def test_protected(self):
+        protected_exists = self.execute('.', 'contains', '_prop')
+
+        self.assertFalse(protected_exists)
+
+    def test_subobject_field(self):
+        field_exists = self.execute('.sub', 'contains', 'field')
+
+        self.assertTrue(field_exists)
+
     def test_method(self):
         method_exists = self.execute('.', 'contains', 'method')
 
         self.assertFalse(method_exists)
+
+
+class GetActionTests(PythonObjectTreeTestsBase):
+    def test_field(self):
+        field_value = self.execute('.field', 'get')
+
+        self.assertEqual(field_value, 'text')
+
+    def test_property(self):
+        property_value = self.execute('.prop', 'get')
+
+        self.assertEqual(property_value, 123)
+
+    def test_subobject_field(self):
+        subobject_field_value = self.execute('.sub.field', 'get')
+
+        self.assertEqual(subobject_field_value, 'SUB')
+
+
+class SetActionTests(PythonObjectTreeTestsBase):
+    def test_field(self):
+        self.execute('.field', 'set', 'NEW')
+
+        self.assertEqual(self.root.field, 'NEW')
+
+    def test_property(self):
+        self.execute('.prop', 'set', 'NEW')
+
+        self.assertEqual(self.root.prop, 'NEW')
